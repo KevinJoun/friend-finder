5ea9d2f9605c5078bdcc6bb9ca2b668f18a4b49e
SilverBell17 <kevinjoun17@gmail.com>
2019-11-14 16:08:29 -0800

    working app

---
+++ app/data/friends.js
@@ -0,0 +1,48 @@
+let friends=
+[{
+    "name": "Kevin",
+    "scores": [
+        5,
+        2,
+        1,
+        3,
+        1,
+        4,
+        5,
+        1,
+        2,
+        3
+    ]
+},
+{
+    "name": "Bob",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+},
+{
+    "name": "Steven",
+    "scores": [
+        1,
+        1,
+        2,
+        5,
+        5,
+        3,
+        1,
+        4,
+        4,
+        4
+    ]
+}]
+
+module.exports = friends
